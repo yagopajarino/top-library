ca8cbc7a317d301f2fd3ccfd0e3457858643e325
Yago Pajariño <yago977@hotmail.com>
2021-10-29 12:58:04 -0300

    main prod

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 let myLibrary = []
 
 function Book(title, author, pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read ? "Already read" : "Not read yet"
+    this.Title = title
+    this.Author = author
+    this.Pages = pages
+    this.Read = read ? "Already read" : "Not read yet"
 }
 
 Book.prototype.info =  function() {
@@ -12,25 +12,104 @@ Book.prototype.info =  function() {
     return `${title} by ${author}, ${pages} pages, ${r}`
 }
 
-function addBookToLibrary(t, a, p, r) {
-    let b = new Book(t,a,p,r)
+function addBookToLibrary(data) {
+    let b = new Book(data[0],data[1],data[2],data[3])
     myLibrary.push(b)
+    displayBooks(myLibrary)
 }
 
+let btn = document.querySelector(".addBtn")
+
 function displayBooks(colection) {
+    if (document.querySelector(".bookContainer") != null) {
+        let d = document.querySelector(".bookContainer")
+        d.remove()
+    }
     let body = document.querySelector("body")
     let container = document.createElement("div")
+    container.classList.toggle("bookContainer")
     myLibrary.forEach(book => {
         let bookCard = document.createElement("div")
+        bookCard.classList.toggle("bookCard")
+        let removeBtn = document.createElement("button")
+        removeBtn.classList.toggle("removeBtn")
+        removeBtn.id = book.Title
+        let readBtn = document.createElement("button")
+        readBtn.classList.toggle("toogleBtn")
+        bookCard.appendChild(removeBtn)
+        bookCard.appendChild(readBtn)
         let infoList = document.createElement("ul")
-        let datos = ["title","author","pages","read"]
+        let datos = ["Title","Author","Pages","Read"]
         datos.forEach(key => {
             let li = document.createElement("li")
-            li.textContent = book[key]
+            li.textContent = `${key}: ${book[key]}`
             infoList.appendChild(li)
         });
         bookCard.appendChild(infoList)
         container.appendChild(bookCard)
     });
-    body.appendChild(container)
+    body.insertBefore(container,btn)
+    addListeners()
+    addReadToggle()
+}
+
+btn.addEventListener("click", toggleForm)
+
+function toggleForm() {
+    let form = document.querySelector(".newBookForm")
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+    form.style.display = "none";
+    }
+}
+
+let cancelBtn = document.querySelector("#cancelBtn")
+cancelBtn.addEventListener("click", event => {
+    toggleForm()
+    cleanForm()
+})
+
+function cleanForm() {
+    let form = document.querySelector(".newBookForm")
+    let inputs = form.querySelectorAll("input")
+    inputs.forEach(element => {
+        element.value = ""
+    });
+    let nop = document.querySelector("#noOption")
+    nop.selected = true
+}
+
+let saveBtn = document.querySelector("#saveBtn")
+saveBtn.addEventListener("click", event => {
+    let form = document.querySelector(".newBookForm")
+    let inputs = form.querySelectorAll("input")
+    let data = []
+    inputs.forEach(element => {
+        data.push(element.value)
+    });
+    let selector = form.querySelectorAll("select")
+    data.push(selector.value)
+    addBookToLibrary(data)
+    toggleForm()
+    cleanForm()
+})
+
+
+function addListeners() {
+    let removeBtns = document.querySelectorAll(".removeBtn")
+    removeBtns.forEach(element => {
+    element.addEventListener("click", event => {
+        myLibrary = myLibrary.filter(book => book.Title != element.id)
+        displayBooks(myLibrary)
+        })
+    });
+};
+
+function addReadToggle() {
+    let toggleBtns = document.querySelectorAll(".toogleBtn")
+    toggleBtns.forEach(element => { 
+        let title = element.parentNode.querySelectorAll("li")[0].textContent.split(":")
+        console.log(title)
+    });
 }
